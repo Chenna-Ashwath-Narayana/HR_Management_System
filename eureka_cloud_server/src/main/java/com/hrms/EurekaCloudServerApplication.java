ddaa0b635601c80475fb484798ae57a528a1e49b
Ashwathayaan <ashwathchenna2002@gmail.com>
2025-03-06 12:39:30 +0530

    Hr Management System

---
+++ eureka_cloud_server/src/main/java/com/hrms/EurekaCloudServerApplication.java
@@ -0,0 +1,13 @@
+package com.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaCloudServerApplication.class, args);
+	}
+
+}
